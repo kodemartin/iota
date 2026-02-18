4489b2fb7223035841c2aab245d2608f44644ef0
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2026-02-18 11:11:47 +0200

    feat(indexer): allocate distinct connection pool to each pipeline

--- crates/iota-indexer/src/test_utils.rs
+++ crates/iota-indexer/src/test_utils.rs
@@ -132,17 +132,18 @@ pub async fn start_test_indexer_impl(
     data_ingestion_path: Option<PathBuf>,
     cancel: CancellationToken,
 ) -> (PgIndexerStore, JoinHandle<Result<(), IndexerError>>) {
-    let store = create_pg_store(&db_url, reset_db);
+    let registry = prometheus::Registry::default();
+    init_metrics(&registry);
+    let indexer_metrics = IndexerMetrics::new(&registry);
+
+    let mut test_db = TestDatabase::new(db_url.clone());
+    let store = create_pg_store(&mut test_db, indexer_metrics.clone(), reset_db);
     if reset_db {
         crate::db::reset_database(&mut store.blocking_cp().get().unwrap()).unwrap();
     }
     if let Some(db_init_hook) = db_init_hook {
         db_init_hook(&store);
     }
-
-    let registry = prometheus::Registry::default();
-    init_metrics(&registry);
-    let indexer_metrics = IndexerMetrics::new(&registry);
 
     let handle = match reader_writer_config {
         IndexerTypeConfig::Reader {
@@ -165,7 +166,6 @@ pub async fn start_test_indexer_impl(
             retention_config,
             optimistic_pruner_batch_size,
         } => {
-            let store_clone = store.clone();
             let mut ingestion_config = IngestionConfig::default();
             ingestion_config.sources.remote_store_url = data_ingestion_path
                 .is_none()
@@ -176,7 +176,8 @@ pub async fn start_test_indexer_impl(
             tokio::spawn(async move {
                 Indexer::start_writer_with_config(
                     &ingestion_config,
-                    store_clone,
+                    db_url,
+                    test_db.pool_config,
                     indexer_metrics,
                     snapshot_config,
                     retention_config,
@@ -203,7 +204,7 @@ pub struct TestDatabase {
     pub url: String,
     db_name: String,
     connection: PoolConnection,
-    pool_config: ConnectionPoolConfig,
+    pub pool_config: ConnectionPoolConfig,
 }
 
 impl TestDatabase {
@@ -257,17 +258,16 @@ impl TestDatabase {
     }
 }
 
-pub fn create_pg_store(db_url: &str, reset_database: bool) -> PgIndexerStore {
-    let registry = prometheus::Registry::default();
-    init_metrics(&registry);
-    let indexer_metrics = IndexerMetrics::new(&registry);
-
-    let mut test_db = TestDatabase::new(db_url.to_string());
+pub fn create_pg_store(
+    test_db: &mut TestDatabase,
+    metrics: IndexerMetrics,
+    reset_database: bool,
+) -> PgIndexerStore {
     if reset_database {
         test_db.recreate();
     }
 
-    PgIndexerStore::new(test_db.to_connection_pool(), indexer_metrics.clone())
+    PgIndexerStore::new(test_db.to_connection_pool(), metrics)
 }
 
 fn replace_db_name(db_url: &str, new_db_name: &str) -> (String, String) {
